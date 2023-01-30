9496945f49d7fa9e1ef27624c6c9c8c9b2dabb9a
rafaelcptx <rafaelteixeiradesigner@gmail.com>
2023-01-29 21:25:05 -0300

    aprendendo mongodb mongoose 1

---
+++ index.js
@@ -0,0 +1,82 @@
+require("dotenv").config();
+let express = require("express");
+let app = express();
+let port = 3080;
+let mongoose = require("mongoose");
+
+// to prepare for the change.
+mongoose.set("strictQuery", false);
+
+// to supress the warning.
+/* mongoose.set('strictQuery', true); */
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+
+let personSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  favoriteFoods: {
+    type: [String],
+  },
+});
+
+let Person = mongoose.model("Person", personSchema);
+
+const createAndSavePerson = (done) => {
+  let rafaelTeixeira = new Person({
+    name: "Rafael Teixeira",
+    age: 24,
+    favoriteFoods: ["Hamburguer"],
+  });
+
+  rafaelTeixeira.save((err, data) => {
+    if (err) console.log(err);
+    done(null, data);
+  });
+};
+
+arrayOfPeople = [
+  { name: "Rafael Teixeira", age: 24, favoriteFoods: ["oldBurger"] },
+  { name: "Lorena", age: 24, favoriteFoods: ["oldBurger"] },
+];
+
+const createManyPeople = (arrayOfPeople, done) => {
+  Person.create(arrayOfPeople, (err, people) => {
+    if (err) console.log(err);
+    done(null, people);
+    console.log(personFound);
+  });
+};
+
+const findPeopleByName = (personName, done) => {
+  Person.find({ name: personName }, (err, personFound) => {
+    if (err) console.log(err);
+    done(null, personFound);
+    console.log(personFound);
+  });
+};
+
+findPeopleByName("Rafael Teixeira", (err, data) => {
+  if (err) console.log(err);
+});
+
+const findOneByFood = (food, done) => {
+  Person.findOne({ favoriteFoods: food }, (err, founded) => {
+    if (err) console.log(err);
+    done(null, founded);
+  });
+};
+
+app.get("/", (req, res) => {
+  res.send(`rodando na porta ${port}`);
+});
+
+app.listen(port);
